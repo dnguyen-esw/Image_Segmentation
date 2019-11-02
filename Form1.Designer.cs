482975080e001353e0200a27f707a3f0ac96a319
hoangduc104 <48839695+hoangduc104@users.noreply.github.com>
2019-11-03 01:40:57 +0700

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace XLA_Project6_Fix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox_hinhgoc = new System.Windows.Forms.PictureBox();
+            this.label_hingoc = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox_seg = new System.Windows.Forms.PictureBox();
+            this.button_seg = new System.Windows.Forms.Button();
+            this.label_X1 = new System.Windows.Forms.Label();
+            this.label_Y1 = new System.Windows.Forms.Label();
+            this.label_Y2 = new System.Windows.Forms.Label();
+            this.label_X2 = new System.Windows.Forms.Label();
+            this.textBox_threshold = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_hinhgoc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_seg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_hinhgoc
+            // 
+            this.pictureBox_hinhgoc.Location = new System.Drawing.Point(12, 33);
+            this.pictureBox_hinhgoc.Name = "pictureBox_hinhgoc";
+            this.pictureBox_hinhgoc.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox_hinhgoc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_hinhgoc.TabIndex = 0;
+            this.pictureBox_hinhgoc.TabStop = false;
+            this.pictureBox_hinhgoc.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_hinhgoc_Paint);
+            this.pictureBox_hinhgoc.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_hinhgoc_MouseDown);
+            this.pictureBox_hinhgoc.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_hinhgoc_MouseMove);
+            this.pictureBox_hinhgoc.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_hinhgoc_MouseUp);
+            // 
+            // label_hingoc
+            // 
+            this.label_hingoc.AutoSize = true;
+            this.label_hingoc.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_hingoc.Location = new System.Drawing.Point(201, 9);
+            this.label_hingoc.Name = "label_hingoc";
+            this.label_hingoc.Size = new System.Drawing.Size(83, 21);
+            this.label_hingoc.TabIndex = 1;
+            this.label_hingoc.Text = "Hinh Goc";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1077, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Segmentation Image";
+            // 
+            // pictureBox_seg
+            // 
+            this.pictureBox_seg.Location = new System.Drawing.Point(899, 33);
+            this.pictureBox_seg.Name = "pictureBox_seg";
+            this.pictureBox_seg.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox_seg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_seg.TabIndex = 2;
+            this.pictureBox_seg.TabStop = false;
+            // 
+            // button_seg
+            // 
+            this.button_seg.Location = new System.Drawing.Point(810, 228);
+            this.button_seg.Name = "button_seg";
+            this.button_seg.Size = new System.Drawing.Size(83, 70);
+            this.button_seg.TabIndex = 4;
+            this.button_seg.Text = "Segmentation";
+            this.button_seg.UseVisualStyleBackColor = true;
+            this.button_seg.Click += new System.EventHandler(this.button_seg_Click);
+            // 
+            // label_X1
+            // 
+            this.label_X1.AutoSize = true;
+            this.label_X1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_X1.Location = new System.Drawing.Point(567, 216);
+            this.label_X1.Name = "label_X1";
+            this.label_X1.Size = new System.Drawing.Size(43, 21);
+            this.label_X1.TabIndex = 5;
+            this.label_X1.Text = "X1: ";
+            // 
+            // label_Y1
+            // 
+            this.label_Y1.AutoSize = true;
+            this.label_Y1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Y1.Location = new System.Drawing.Point(652, 216);
+            this.label_Y1.Name = "label_Y1";
+            this.label_Y1.Size = new System.Drawing.Size(41, 21);
+            this.label_Y1.TabIndex = 6;
+            this.label_Y1.Text = "Y1: ";
+            // 
+            // label_Y2
+            // 
+            this.label_Y2.AutoSize = true;
+            this.label_Y2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Y2.Location = new System.Drawing.Point(652, 267);
+            this.label_Y2.Name = "label_Y2";
+            this.label_Y2.Size = new System.Drawing.Size(41, 21);
+            this.label_Y2.TabIndex = 8;
+            this.label_Y2.Text = "Y2: ";
+            // 
+            // label_X2
+            // 
+            this.label_X2.AutoSize = true;
+            this.label_X2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_X2.Location = new System.Drawing.Point(567, 267);
+            this.label_X2.Name = "label_X2";
+            this.label_X2.Size = new System.Drawing.Size(43, 21);
+            this.label_X2.TabIndex = 7;
+            this.label_X2.Text = "X2: ";
+            // 
+            // textBox_threshold
+            // 
+            this.textBox_threshold.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_threshold.Location = new System.Drawing.Point(701, 148);
+            this.textBox_threshold.Name = "textBox_threshold";
+            this.textBox_threshold.Size = new System.Drawing.Size(69, 29);
+            this.textBox_threshold.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(655, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 21);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "D0:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_threshold);
+            this.Controls.Add(this.label_Y2);
+            this.Controls.Add(this.label_X2);
+            this.Controls.Add(this.label_Y1);
+            this.Controls.Add(this.label_X1);
+            this.Controls.Add(this.button_seg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox_seg);
+            this.Controls.Add(this.label_hingoc);
+            this.Controls.Add(this.pictureBox_hinhgoc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_hinhgoc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_seg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_hinhgoc;
+        private System.Windows.Forms.Label label_hingoc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox_seg;
+        private System.Windows.Forms.Button button_seg;
+        private System.Windows.Forms.Label label_X1;
+        private System.Windows.Forms.Label label_Y1;
+        private System.Windows.Forms.Label label_Y2;
+        private System.Windows.Forms.Label label_X2;
+        private System.Windows.Forms.TextBox textBox_threshold;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XLA_Project6_Fix
+{
+    public partial class Form1 : Form
+    {
+        Bitmap hinhgoc;
+        Rectangle Rect;
+        Point LocationX1Y1;//chứa giá trị vị trí X1, Y1 trên hình
+        Point LocationX2Y2;//chứa giá trị vị trí X2, Y2 trên hình
+        bool IsMouseDown = false;//check chuột đã được nhấn hay chưa, kéo thả giống paint
+        bool IsMouseUp = false;//check chuột đã được thả hay chưa
+        public Form1()
+        {
+            InitializeComponent();
+            hinhgoc = new Bitmap(@"C:\Users\hoang\OneDrive\Máy tính\Image Processing\C#\lena_color.gif");
+            pictureBox_hinhgoc.Image = hinhgoc;
+        }
+
+        private void pictureBox_hinhgoc_MouseDown(object sender, MouseEventArgs e)//event click chuột trái
+        {
+            IsMouseDown = true;
+            LocationX1Y1 = e.Location;//lấy cặp vị trí X,Y của trỏ chuột trong hình, chính là vị trí (x,y) của pixel ngay trỏ chuột
+            label_X1.Text = "X1: "+LocationX1Y1.X.ToString();//chuyển thành chuỗi để hiển thị
+            label_Y1.Text = "Y1: "+LocationX1Y1.Y.ToString();
+        }
+
+        private void pictureBox_hinhgoc_MouseMove(object sender, MouseEventArgs e)//event rê trỏ chuột khi trong khi chuột trái còn nhấn
+        {
+            if(IsMouseDown==true)//check chuột trái còn nhấn thì refresh giá trị x,y liên tục khi rê chuột
+            {
+                LocationX2Y2 = e.Location;
+                label_X2.Text = "X2: "+LocationX2Y2.X.ToString();
+                label_Y2.Text = "Y2: "+LocationX2Y2.Y.ToString();
+                Refresh();
+            }
+        }
+
+        private void pictureBox_hinhgoc_MouseUp(object sender, MouseEventArgs e)//event thả chuột để lấy vị trí pixel cuối
+        {
+            if (IsMouseDown == true)
+            {
+                LocationX2Y2 = e.Location;
+                label_X2.Text = "X2: "+LocationX2Y2.X.ToString();
+                label_Y2.Text = "Y2: "+LocationX2Y2.Y.ToString();
+                IsMouseDown = false;
+                IsMouseUp = true;
+            }
+        }
+
+        private void pictureBox_hinhgoc_Paint(object sender, PaintEventArgs e)//vẽ vùng rê chuột hình chữ nhật có tọa độ điểm đầu cuối (X1,Y1)-(X2,Y2)
+        {
+            if(Rect!=null)
+            {
+                e.Graphics.DrawRectangle(Pens.Red, GetRect());
+            }
+        }
+
+        private Rectangle GetRect()//hàm lấy giá trị của vùng đã chọn để vẽ 1 hình chữ nhật
+        {
+            Rect = new Rectangle();
+            Rect.X = Math.Min(LocationX1Y1.X, LocationX2Y2.X);
+            Rect.Y = Math.Min(LocationX1Y1.Y, LocationX2Y2.Y);
+            Rect.Width = Math.Abs(LocationX1Y1.X - LocationX2Y2.X);
+            Rect.Height = Math.Abs(LocationX1Y1.Y - LocationX2Y2.Y);
+            return Rect;
+        }
+
+        double[] VectorA(Bitmap Hinhgoc,int X1,int Y1,int X2,int Y2)
+        {
+            double[] a = new double[3];//vector màu trung bình-giá trị trung bình các kênh R-G-B trong vùng màu đã chọn
+            a[0] = 0;
+            a[1] = 0;
+            a[2] = 0;
+            int temp=0;//biến tạm dùng xử lý 2 if bên dưới
+
+            if (X1 > X2)//X1>X2 for dưới sẽ báo lỗi
+            {
+                temp = X1;
+                X1 = X2;
+                X2 = temp;
+            }
+            if (Y1 > Y2)
+            {
+                temp = Y1;
+                Y1 = Y2;
+                Y2 = temp;
+            }
+
+            for (int x = X1; x <= X2; x++)
+                for(int y = Y1; y <= Y2; y++)
+                {
+                    Color c = Hinhgoc.GetPixel(x, y);
+                    byte R = c.R;
+                    byte G = c.G;
+                    byte B = c.B;
+                    a[0] += R;//cộng dồn giá trị màu đỏ của tất cả pixel trong vùng màu trung bình
+                    a[1] += G;//_____________________green_______________________________________
+                    a[2] += B;//_____________________blue_________________________________________
+                }
+            double size = (X2 - X1) * (Y2 - Y1);//size của vùng ảnh đã chọn, là tổng số pixel trong vùng màu đã chọn(hình chữ nhật) 
+            a[0] = a[0] / size;//giá trị màu đỏ trung bình
+            a[1] = a[1] / size;//giá trị màu green trung bình
+            a[2] = a[2] / size;//____________blue________
+
+            return a;//trả về vector màu trung bình của vùng đã chọn, gồm a[0], a[1], a[2]
+        }
+
+        Bitmap Segmentation(Bitmap Hinhgoc,double[] a,int D0)
+        {
+            Bitmap Seg = new Bitmap(Hinhgoc.Width,hinhgoc.Height);
+            
+            for(int x=0;x<Hinhgoc.Width;x++)
+                for(int y=0;y<Hinhgoc.Height;y++)
+                {
+                    Color color = Hinhgoc.GetPixel(x, y);
+                    byte zR = color.R;
+                    byte zG = color.G;
+                    byte zB = color.B;
+
+                    double D = 0;
+                    D = Math.Sqrt(Math.Pow(zR - a[0], 2) + Math.Pow(zG - a[1], 2) + Math.Pow(zB - a[2], 2));//euclidean distance
+
+                    if (D <= Convert.ToInt32(textBox_threshold.Text))//chuyển từ kiểu string trong textbox về dạng Integer
+                        Seg.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                    else
+                        Seg.SetPixel(x, y, Color.FromArgb(zR, zG, zB));
+
+                }
+            return Seg;
+        }
+
+        private void button_seg_Click(object sender, EventArgs e)
+        {
+            if(String.IsNullOrEmpty(textBox_threshold.Text))//nếu textbox chưa nhập giá trị hoặc chứa khoảng trống thì hiện thông báo
+            {
+                MessageBox.Show("Nhap D0");
+            }
+
+            if(IsMouseUp==true)//việc phân đoạn ảnh sẽ đc gọi sau khi thả chuột ở vị trí X2,Y2
+            {
+                double[] av = new double[3];
+                av=VectorA(hinhgoc, LocationX1Y1.X, LocationX1Y1.Y, LocationX2Y2.X, LocationX2Y2.Y);
+
+                if (String.IsNullOrEmpty(textBox_threshold.Text)==false)//nếu textbox có giá trị thì mới thực hiện phân đoạn
+                {
+                    pictureBox_seg.Image = Segmentation(hinhgoc, av, Convert.ToInt32(textBox_threshold.Text));
+                }
+            }
+            
+        }
+    }
+}
